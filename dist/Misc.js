d1c085751985b55bd71b1300534f34cf183b7521
Jan Johansen <jan@johansenweb.dk>
2022-02-15 09:40:47 +0100

    Delete dist from GIT!

--- dist/Misc.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// {
-// 	"1609316316772": { "type": "Flow", "ins": {}, "outs": {}, "nodes": {}, "connections": {}, "_id": "1609316316772" },
-// 	"1609316316772_2": { "type": "FlowNode", "nodeType": "Ticker", "_id": "1609316316772_" }
-// }
--- dist/common/flowTypes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-//
-// NOTE TO SELF: Files are copied from back/common --> front/common (using cpx in dev script)
-//
-Object.defineProperty(exports, "__esModule", { value: true });
